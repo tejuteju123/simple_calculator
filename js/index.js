7ad1a99bb78efde5e4ccd8dd90d84fcc805743f3
tejuteju123 <188r1a04h7@gmail.com>
2022-03-05 19:39:58 -0800

    genc _stage1simple calculator

---
+++ js/index.js
@@ -0,0 +1,6 @@
+  //back space code 
+  var backspace = function() {
+    var var_name  = document.getElementById('d').value;
+    document.getElementById('d').value=var_name.substring(0,var_name.length -1);
+  
+  }
